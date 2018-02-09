f4dde22c1981a5828ce620aa88a264d5be91e26a
bradito <bradito@mac.com>
2018-02-08 21:09:20 -0600

    first multithread webserver attempt

--- PiPyRunner.py
+++ PiPyRunner.py
@@ -2,10 +2,23 @@
 from flask import Flask, render_template
 from astral import Astral
 import datetime
-import light_programs
+import threading
+import time
 
+#
 #create object of our strip
 #create thread event and laucncher
+
+stopEvent = threading.Event()
+lightsThread = None
+
+
+def printoften(e):
+	while not e.isSet():
+		now = datetime.datetime.now()
+		timestring = now.strftime("%Y-%m-%d %H:%M")
+		print('The current time', timestring)
+		time.sleep(4)
 
 app = Flask(__name__)
 
@@ -14,7 +27,7 @@ def homepage():
 	now = datetime.datetime.now()
 	timestring = now.strftime("%Y-%m-%d %H:%M")
 	#Add a list of programs to display
-	return render_template('main.html', **templateData)
+	return render_template('main.html', timestring=timestring, message = "")
 
 
 @app.route('/update/<action>')
@@ -22,7 +35,34 @@ def update(action):
 	"""set a new program from the letter in the URL"""
 	now = datetime.datetime.now()
 	timestring = now.strftime("%Y-%m-%d %H:%M")
-	return render_template('main.html', **templateData)
+	return render_template('main.html', timestring=timestring, message = "")
+
+@app.route('/start')
+def startpage():
+	global stopEvent
+	global lightsThread
+	t1 = threading.Thread(target=printoften,
+						  args=(stopEvent,))
+	t1.start()
+	print("started")
+	lightsThread = t1
+	now = datetime.datetime.now()
+	timestring = now.strftime("%Y-%m-%d %H:%M")
+	return render_template('main.html', timestring=timestring, message = ("thread started:",t1))
+
+@app.route('/stop')
+def stoppage():
+	global stopEvent
+	global lightsThread
+	stopEvent.set()
+	print("stopping soon")
+	lightsThread.join()
+
+	now = datetime.datetime.now()
+	timestring = now.strftime("%Y-%m-%d %H:%M")
+	
+	stopEvent.clear()
+	return render_template('main.html', timestring=timestring, message = ("thread stopped:",lightsThread))
 
 
 @app.context_processor
@@ -44,7 +84,7 @@ def sunset():
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=False)
+	app.run(host='0.0.0.0', port=8000, debug=False)
 
 
 
